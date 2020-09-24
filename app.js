9a6cc45b439d8f8ae9d3d390c91e96a83a887f93
Douglas Cidral <doug.coding20@gmail.com>
2020-09-24 10:42:04 -0300

    Adding the Mongoose Schema

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const userRouter = require('./routes/userRouter');
 const mongoose = require('mongoose');
 
-mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true}, (error)=>{
+    if(error) {console.log(error);} else { console.log('Mongo Connect');}
+})
 
-app.use('/user', userRouter);
+app.use('/user', express.json(), userRouter);
 
 app.listen(process.env.PORT, ()=>{ console.log("Running")});
