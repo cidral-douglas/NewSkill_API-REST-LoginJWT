2d0570801bde03a108316dd7e09944541b6a0c62
Douglas Cidral <doug.coding20@gmail.com>
2020-09-24 09:45:47 -0300

    gitignore

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter');
+const mongoose = require('mongoose');
+
+mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
 
 app.use('/user', userRouter);
 
